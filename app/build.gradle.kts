ca5cce45880491fbafe26448d87acf7841029217
tiago233tek-spec <tiago233tek@gmail.com>
2026-08-31 02:04:07 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")}
-dandroid {
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
     namespace = "com.demenor.gameturbo"
     compileSdk = 35
 
@@ -19,11 +21,15 @@ dandroid {
         versionName = "1.0"
     }
 }
-ependencies {
+
+dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+    
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
